176c0b3cf6a06bf8c9348d62505bd3f789cd7bf6
nowy21@onet.com.pl <nowy21@onet.com.pl>
2021-06-20 23:40:37 +0200

    code cleaning

--- notifications/src/main/java/com/example/demo/service/EmailSenderImpl.java
+++ notifications/src/main/java/com/example/demo/service/EmailSenderImpl.java
@@ -1,7 +1,5 @@
 package com.example.demo.service;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
